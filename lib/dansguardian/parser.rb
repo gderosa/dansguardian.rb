ed6510f34ceb4f6431b75a1bc5fa2188bd06b3c8
Guido De Rosa <job@guidoderosa.net>
2010-09-15 09:25:44 +0200

    split files

---
+++ lib/dansguardian/parser.rb
@@ -0,0 +1,35 @@
+module DansGuardian
+  module Parser
+    def self.read(io)
+      h = {}
+      may_appear_multiple_times = [
+        :downloadmanager, :contentscanner, :authplugin]
+      io.each_line do |line|
+        line.sub! /#.*$/, ''
+        line.strip!
+        match = false
+        case line
+        when /^([^=\s]+)\s*=\s*([^=\s']*)$/ 
+          key = $1.to_sym
+          value = $2
+          match = true
+        when /^([^=\s]+)\s*=\s*'(.*)'$/
+          key = $1.to_sym
+          value = $2.gsub(/\\'/, "'")
+          match = true
+        end
+        if match
+          if may_appear_multiple_times.include? key 
+            h[key] ||= []
+            h[key] << value
+          else
+            h[key] = value
+          end
+        end
+      end
+      return h
+    end
+  end
+end
+
+
