f8bdb1128da49a7da095df23d7cf48e74403d34f
Guido De Rosa <guido.derosa@vemarsas.it>
2011-02-17 18:08:43 +0000

    List#each_line (wip? useful?)

--- lib/dansguardian/list.rb
+++ lib/dansguardian/list.rb
@@ -42,5 +42,27 @@ module DansGuardian
       end
     end
 
+    def each_line(opts={:exclude => []}) 
+      Enumerator.new do |yielder|
+        File.foreach(@init[:file]) do |line|
+          line.strip!
+          line.force_encoding file_encoding
+          case line
+          when /^#listcategory:\s*"(.*)"/
+            next if opts[:exclude].include? :listcategory
+          when /^[\s#]*\.Include/
+            next if opts[:exclude].include? :includes
+          when /^[\s#]*</
+            next if opts[:exclude].include? :items
+          when /^\s*#/
+            next if opts[:exclude].include? :comment_lines
+          when /^\s*$/
+            next if opts[:exclude].include? :blank_lines
+          end
+          yielder.yield line 
+        end
+      end
+    end
+
   end
 end
